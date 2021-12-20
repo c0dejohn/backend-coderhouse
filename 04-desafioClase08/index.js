b67790cd36efc3d0288663b853d57cb01845f8ea
c0dejohn <jemanyoma07@gmail.com>
2021-12-20 18:21:11 -0500

    update

--- 04-desafioClase08/index.js
+++ 04-desafioClase08/index.js
@@ -1,40 +1,14 @@
 const express = require('express')
 const app = express()
-const apiRouter = express.Router()
+const apiRouter = require('./routes/productos.routes')
 const PORT = 8080
-
-const Producto = require('./productos')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api', apiRouter)
+
 app.use('/static', express.static(__dirname + '/public'))
 
-apiRouter.get('/productos', (req, res) => {
-  res.send(Producto.listarProducto)
-})
-
-apiRouter.post('/productos', (req, res) => {
-  let toAdd = req.body
-  let prod = Producto.nuevoProducto(toAdd)
-  res.send(prod)
-})
-
-apiRouter.get('/productos/:id', (req, res) => {
-  let id = req.params.id
-  res.send(Producto.mostrarProducto(id))
-})
-
-apiRouter.put('/productos/:id', (req, res) => {
-  let toChange = req.body
-  let id = req.params.id
-  res.send(Producto.actualizarProducto(toChange, id))
-})
-
-apiRouter.delete('/productos/:id', (req, res) => {
-  let id = req.params.id
-  res.send(Producto.eliminarProducto(id))
-})
+app.use('/api', apiRouter)
 
 app.listen(PORT, (err) => {
   if (err) {

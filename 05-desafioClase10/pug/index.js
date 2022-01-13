a67fd211f13383a907b5ad8ebcbeb41833594dbb
c0dejohn <jemanyoma07@gmail.com>
2022-01-13 00:56:56 -0500

    updated

---
+++ 05-desafioClase10/pug/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const path = require('path')
+const app = express()
+const apiRouter = require('./routes/productos.routes')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+//handlebars settings
+
+app.engine('pug', require('pug').__express)
+
+app.set('views', './views/')
+app.set('view engine', 'pug')
+
+app.use('/', apiRouter)
+app.use(express.static('public'))
+
+const PORT = 8080
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Server listening on PORT', PORT)
+  }
+})
